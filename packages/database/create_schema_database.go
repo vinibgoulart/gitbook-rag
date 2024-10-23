bf9cc5b9ab5ae0a320540fc5ffe215f87cfdaabb
Vinicius <viblaziusgoulart@gmail.com>
2024-10-23 09:41:03 -0300

    feat(models): add chat and session models in creation

--- packages/database/create_schema_database.go
+++ packages/database/create_schema_database.go
@@ -4,8 +4,10 @@ import (
 	"context"
 
 	"github.com/uptrace/bun"
+	"github.com/vinibgoulart/gitbook-rag/packages/chat"
 	"github.com/vinibgoulart/gitbook-rag/packages/content"
 	"github.com/vinibgoulart/gitbook-rag/packages/page"
+	"github.com/vinibgoulart/gitbook-rag/packages/session"
 	"github.com/vinibgoulart/gitbook-rag/packages/space"
 )
 
@@ -14,6 +16,8 @@ func CreateSchemaDatabase(db *bun.DB, ctx context.Context) error {
 		(*space.Space)(nil),
 		(*content.Content)(nil),
 		(*page.Page)(nil),
+		(*session.Session)(nil),
+		(*chat.Chat)(nil),
 	}
 
 	for _, model := range models {
