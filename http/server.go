c952957f3048f2b5b2472e7c65a7d4f6057e8cc7
Vinicius <viblaziusgoulart@gmail.com>
2024-10-23 09:40:38 -0300

    chore(path): path api page handler

--- http/server.go
+++ http/server.go
@@ -11,7 +11,7 @@ import (
 	"github.com/go-chi/chi"
 	"github.com/go-chi/chi/middleware"
 	"github.com/uptrace/bun"
-	page "github.com/vinibgoulart/gitbook-rag/packages/page/handler"
+	page "github.com/vinibgoulart/gitbook-rag/packages/page/handler/api"
 )
 
 func ServerInit(db *bun.DB) func(ctx context.Context, waitGroup *sync.WaitGroup) {
