34a1faebed3e59aed166c2ee161b658e0f2eaa63
Vinicius <viblaziusgoulart@gmail.com>
2024-10-25 14:29:54 -0300

    feat(openai): improve system message to be more friendly

--- packages/openai/generate_completion.go
+++ packages/openai/generate_completion.go
@@ -2,20 +2,23 @@ package openai
 
 import (
 	"context"
+	"fmt"
 
 	"github.com/openai/openai-go"
 )
 
 func GenerateCompletion(ctx *context.Context) func(context *string, messages ...openai.ChatCompletionMessageParamUnion) (string, error) {
 	return func(context *string, messages ...openai.ChatCompletionMessageParamUnion) (string, error) {
+		systemMessage := openai.SystemMessage(fmt.Sprintf("You are a helpful assistant that uses the provided context as reference, but can answer questions that are outside the scope of the context in a natural way.: %s", *context))
+
 		allMessages := append([]openai.ChatCompletionMessageParamUnion{
-			openai.SystemMessage(*context),
+			systemMessage,
 		}, messages...)
 
 		chatCompletion, err := Client.Chat.Completions.New(*ctx, openai.ChatCompletionNewParams{
 			Messages:    openai.F(allMessages),
 			Model:       openai.F(openai.ChatModelGPT4o),
-			Temperature: openai.Float(0.3),
+			Temperature: openai.Float(1),
 		})
 
 		if err != nil {
