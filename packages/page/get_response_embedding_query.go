49d24fdc3427f0b324903218b24898fe856769e0
Vinicius <viblaziusgoulart@gmail.com>
2024-10-31 20:59:51 -0300

    feat(response): response gpt when do not find items

--- packages/page/get_response_embedding_query.go
+++ packages/page/get_response_embedding_query.go
@@ -3,7 +3,6 @@ package page
 import (
 	"context"
 	"fmt"
-	"os"
 
 	"github.com/google/uuid"
 	"github.com/openai/openai-go"
@@ -52,7 +51,7 @@ func GetResponseEmbeddingQuery(ctx *context.Context, db *bun.DB) func(query *str
 			}
 
 			if len(items) == 0 {
-				return os.Getenv("CHATBOT_MESSAGE_NOT_FOUND"), nil
+				return generateResponse(ctx, db)(query, &session.Context)
 			}
 
 			session.Context = items[0].Text
